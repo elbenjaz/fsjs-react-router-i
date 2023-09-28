b17fd1062c03ea14efda879a0801db0d541e441e
Benjamín Segura Hernández <benjamin.segura.hernandez@gmail.com>
2023-09-27 21:03:46 -0300

    - url deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://elbenjaz.github.io/fsjs-react-router-i/'
+//  base: 'https://elbenjaz.github.io/fsjs-react-router-i/'
 })
